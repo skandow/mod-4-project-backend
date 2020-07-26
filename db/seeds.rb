4a003ad073c4aa2ec251eec0da7ae0ae705531ec
skandow <kando1sr@gmail.com>
2020-07-26 12:38:37 -0500

    database setup complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+newUser = User.create(username: "MyFirstUser", email: "user1@user.com", age: 25, gender: "male", image_url: "none")
+
+note1 = Note.create(title: "My First Note", content: "What a great note!", completed: false, starred: false, user_id: 1)
+note2 = Note.create(title: "My Second Note", content: "This is still a good note!", completed: false, starred: true, user_id: 1)
