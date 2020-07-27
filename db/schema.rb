185e17f8622384d01becb7632fc87126f2eca3c2
skandow <kando1sr@gmail.com>
2020-07-27 10:38:29 -0500

    attempted authentication on back end

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_173057) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "notes", force: :cascade do |t|
-    t.string "title"
-    t.string "content"
-    t.boolean "completed"
-    t.boolean "starred"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.integer "age"
-    t.string "gender"
-    t.string "image_url"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
