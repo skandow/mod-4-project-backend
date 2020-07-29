d842ca69eb4997168417feac2382a03c04ca8d40
skandow <kando1sr@gmail.com>
2020-07-28 19:22:52 -0500

    minor edit

--- app/serializers/note_serializer.rb
+++ app/serializers/note_serializer.rb
@@ -1,5 +1,5 @@
 class NoteSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :title, :content, :completed, :starred, :user_id
+  attributes :id, :title, :content, :completed, :starred, :user_id, :created_at
   belongs_to :user
 end
