85c45e0e05d537ab529c89dad1f8bd4343b09af5
skandow <kando1sr@gmail.com>
2020-07-31 10:15:12 -0500

    readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,21 +10,7 @@ require 'faker'
 require 'database_cleaner'
 DatabaseCleaner.clean_with(:truncation)
 
-
 newUser = User.create(username: "Steven", password: "pa$sword", email: "user1@user.com", age: 34, gender: "male", image_url: Faker::Avatar.image)
-
-# note1 = Note.create(title: "My First Note", content: "What a great note!", starred: false, user_id: 1)
-# note2 = Note.create(title: "My Second Note", content: "This is still a good note!", starred: true, user_id: 1)
-
-
-
-# User.delete_all
-# Note.delete_all
-# Course.delete_all
-
-# ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'users'")
-# ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'notes'")
-# ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'courses'")
 
 25.times do 
     rando = rand(18..40)
@@ -37,30 +23,11 @@ newUser = User.create(username: "Steven", password: "pa$sword", email: "user1@us
         )
 end 
 
-TITLES = ["The Meaning of Life", "Gone With the Wind", "My Teacher's Favorite Thing", "Why Am I Here?", "Enough of This", "When is the Exam?", "I'm So Sleepy..."]
-VERDICT1 = [true, false, false, false, false]
-
-# User.all.each do |user|
-#     dates_array = []
-#     date = Date.today - 20
-#     while date != Date.today
-#         dates_array << date.strftime("%A, %B %-d, %Y")
-#         date += 1 
-#     end 
-#     dates_array.each do |date|
-#         Note.create(title: TITLES.sample, content: Faker::Movie.quote, starred: VERDICT1.sample, user_id: User.all.sample.id)
-#     end
-# end  
+TITLES = ["Enumerables in Ruby", "Using Sinatra", "Redux Thunk: What I Thunk About It", "Client Side Routing", "Using Rails as an API", "React Components", "What is React Native"]
+VERDICT = [true, false, false, false, false]
 
 User.all.each do |user|
     TITLES.each do |title|
-        Note.create(title: title, content: Faker::Movie.quote, starred: VERDICT1.sample, user_id: user.id)
+        Note.create(title: title, content: Faker::Lorem.paragraphs, starred: VERDICT.sample, user_id: user.id)
     end
 end  
-
-# firstLog = DailyLog.create(status: "complete", title: "Sunday, July 5, 2020")
-# secondLog = DailyLog.create(status: "complete", title: "Monday, July 6, 2020")
-# thirdLog = DailyLog.create(status: "complete", title: "Tuesday, July 7, 2020")
-
-
-# VERDICT2 = [true, false, false, false, false, false, false, false, false, false]
